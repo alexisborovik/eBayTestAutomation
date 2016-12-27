afebbdde39cd93ed035a236bd86db26c12440e59
Alexis Borovik <borovikalexey@mail.ru>
2016-12-27 20:56:55 +0300

    Code refractoring

--- EbayTestAutomation/EbayTests.cs
+++ EbayTestAutomation/EbayTests.cs
@@ -33,69 +33,99 @@ namespace EbayTestAutomation
         [Test]
         public void Register()
         {
-            steps.Register(settings["EMAIL"], settings["EMAIL"], settings["FIRSTNAME"], settings["LASTNAME"],
-                settings["PASS"], settings["PHONE"], settings["COUNTRY"]);
+            steps.Register(settings["EMAIL"],
+                settings["EMAIL"],
+                settings["FIRSTNAME"],
+                settings["LASTNAME"],
+                settings["PASS"],
+                settings["PHONE"],
+                settings["COUNTRY"]);
             Assert.True(steps.GoToShopping());
         }
 
         [Test]
         public void RegisterWithDifferentEmails()
         {
-            steps.Register(settings["EMAIL"], settings["SECOND_EMAIL"], settings["FIRSTNAME"], settings["LASTNAME"],
-                settings["PASS"], settings["PHONE"], settings["COUNTRY"]);
+            steps.Register(settings["EMAIL"],
+                settings["SECOND_EMAIL"],
+                settings["FIRSTNAME"],
+                settings["LASTNAME"],
+                settings["PASS"],
+                settings["PHONE"],
+                settings["COUNTRY"]);
             Assert.True(steps.IsRegisterEmailsDontMatch());
         }
 
         [Test]
         public void RegisterWithIncorrectEmail()
         {
-            steps.Register(settings["INCORRECT_EMAIL"], settings["INCORRECT_EMAIL"], settings["FIRSTNAME"], settings["LASTNAME"],
-                settings["PASS"], settings["PHONE"], settings["COUNTRY"]);
+            steps.Register(settings["INCORRECT_EMAIL"],
+                settings["INCORRECT_EMAIL"],
+                settings["FIRSTNAME"], 
+                settings["LASTNAME"],
+                settings["PASS"], 
+                settings["PHONE"],
+                settings["COUNTRY"]);
             Assert.True(steps.IsRegisterEmailIncorrect());
         }
 
         [Test]
         public void RegisterWithPassEqualsToEmail()
         {
-            steps.Register(settings["EMAIL"], settings["EMAIL"], settings["FIRSTNAME"], settings["LASTNAME"],
-                settings["EMAIL"], settings["PHONE"], settings["COUNTRY"]);
+            steps.Register(settings["EMAIL"],
+                settings["EMAIL"], 
+                settings["FIRSTNAME"],
+                settings["LASTNAME"],
+                settings["EMAIL"], 
+                settings["PHONE"],
+                settings["COUNTRY"]);
             Assert.True(settings["ERR_PASS_MATCH_TO_NAME"].Contains(steps.GetRegisterPassErrMessage()));
         }
 
         [Test]
         public void RegisterWithPassContainsOnlyDigits()
         {
-            steps.Register(settings["EMAIL"], settings["EMAIL"], settings["FIRSTNAME"], settings["LASTNAME"], 
-                settings["PASS_CONTAINS_ONLY_DIGITS"], settings["PHONE"], settings["COUNTRY"]);
+            steps.Register(settings["EMAIL"], 
+                settings["EMAIL"], 
+                settings["FIRSTNAME"],
+                settings["LASTNAME"], 
+                settings["PASS_CONTAINS_ONLY_DIGITS"],
+                settings["PHONE"], 
+                settings["COUNTRY"]);
             Assert.True(settings["ERR_PASS_REQUIREMENTS"].Contains(steps.GetRegisterPassErrMessage()));
         }
 
         [Test]
         public void RegisterWithShortPass()
         {
-            steps.Register(settings["EMAIL"], settings["EMAIL"], settings["FIRSTNAME"], settings["LASTNAME"],
-                settings["SHORT_PASS"], settings["PHONE"], settings["COUNTRY"]);
+            steps.Register(settings["EMAIL"],
+                settings["EMAIL"],
+                settings["FIRSTNAME"],
+                settings["LASTNAME"],
+                settings["SHORT_PASS"],
+                settings["PHONE"],
+                settings["COUNTRY"]);
             Assert.True(settings["ERR_PASS_TOO_SHORT"].Contains(steps.GetRegisterPassErrMessage()));
         }
 
         [Test]
         public void SignInViaEmail()
         {
-            steps.SignIn(settings["EMAIL"],settings["PASS"], false);
+            steps.SignIn(settings["EMAIL"], settings["PASS"], false);
             Assert.True(steps.IsSignedIn());
         }
 
         [Test]
         public void SignInViaLogin()
         {
-            steps.SignIn(settings["LOGIN"],settings["PASS"], false);
+            steps.SignIn(settings["LOGIN"], settings["PASS"], false);
             Assert.True(steps.IsSignedIn());
         }
 
         [Test]
         public void CheckLogout()
         {
-            steps.SignIn(settings["EMAIL"],settings["PASS"], true);
+            steps.SignIn(settings["EMAIL"], settings["PASS"], true);
             steps.TryLogout();
             Assert.False(steps.IsSignedIn());
         }
@@ -111,7 +141,7 @@ namespace EbayTestAutomation
         [Test]
         public void AddToCart()
         {
-            steps.SignIn(settings["EMAIL"],settings["PASS"], false);
+            steps.SignIn(settings["EMAIL"], settings["PASS"], false);
             steps.Search(settings["SEARCH_REQUEST"]);
             string itemTitle = steps.GoToSearchResult(1);
             steps.AddToCart();
@@ -122,7 +152,7 @@ namespace EbayTestAutomation
         [Test]
         public void RemoveFromCart()
         {
-            steps.SignIn(settings["EMAIL"],settings["PASS"], false);
+            steps.SignIn(settings["EMAIL"], settings["PASS"], false);
             steps.Search(settings["SEARCH_REQUEST_FOR_REMOVE"]);
             string itemTitle = steps.GoToSearchResult(1);
             steps.AddToCart();
@@ -133,7 +163,7 @@ namespace EbayTestAutomation
         [Test]
         public void SaveForLater()
         {
-            steps.SignIn(settings["EMAIL"],settings["PASS"], false);
+            steps.SignIn(settings["EMAIL"], settings["PASS"], false);
             steps.Search(settings["SEARCH_REQUEST"]);
             string itemTitle = steps.GoToSearchResult(1);
             steps.AddToCart();
@@ -145,7 +175,7 @@ namespace EbayTestAutomation
         [Test]
         public void BackFromLaterToCart()
         {
-            steps.SignIn(settings["EMAIL"],settings["PASS"], false);
+            steps.SignIn(settings["EMAIL"], settings["PASS"], false);
             steps.Search(settings["SEARCH_REQUEST"]);
             string itemTitle = steps.GoToSearchResult(1);
             steps.AddToCart();
@@ -157,7 +187,7 @@ namespace EbayTestAutomation
         [Test]
         public void RemoveFromLater()
         {
-            steps.SignIn(settings["EMAIL"],settings["PASS"], false);
+            steps.SignIn(settings["EMAIL"], settings["PASS"], false);
             steps.Search(settings["SEARCH_REQUEST"]);
             string itemTitle = steps.GoToSearchResult(1);
             steps.AddToCart();
